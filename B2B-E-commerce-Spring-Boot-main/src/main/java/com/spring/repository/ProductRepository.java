3e8b50c5a0439b7c4f93673ac55a9c025ed03cd5
MD. JAHANGIR ALAM <mdxahangir@gmail.com>
2025-09-07 23:03:31 +0600

    Update ProductRepository.java

--- B2B-E-commerce-Spring-Boot-main/src/main/java/com/spring/repository/ProductRepository.java
+++ B2B-E-commerce-Spring-Boot-main/src/main/java/com/spring/repository/ProductRepository.java
@@ -1,17 +1,3 @@
-//package com.spring.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import com.spring.model.Product;
-//
-//import jakarta.transaction.Transactional;
-//
-//@Repository("productRepository")
-//@Transactional
-//public interface ProductRepository extends JpaRepository<Product, Long> {
-//
-//}
 package com.spring.repository;
 
 import com.spring.model.Product;
