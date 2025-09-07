fd936492c8e22b775e70141fc3b674ff076894a0
MD. JAHANGIR ALAM <mdxahangir@gmail.com>
2025-09-07 22:59:58 +0600

    Update ProductService.java

--- B2B-E-commerce-Spring-Boot-main/src/main/java/com/spring/service/ProductService.java
+++ B2B-E-commerce-Spring-Boot-main/src/main/java/com/spring/service/ProductService.java
@@ -1,24 +1,3 @@
-//package com.spring.service;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import com.spring.model.Product;
-//import com.spring.repository.ProductRepository;
-//
-//@Service
-//public class ProductService {
-//    
-//    @Autowired
-//    private ProductRepository productRepository;
-//    
-//    public Product getProductById(Long id) {
-//        return productRepository.findById(id)
-//                .orElseThrow(() -> new RuntimeException("Product not found"));
-//    }
-//    
-//    // Other product related methods...
-//}
 package com.spring.service;
 
 import com.spring.model.Product;
